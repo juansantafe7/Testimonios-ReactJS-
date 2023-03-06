557658a165173bff5c90aa354afb9fa063c2e349
Juan <juanleal.santafe@gmail.com>
2023-03-06 15:27:06 -0300

    primer-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+
+          <h1>Estos son nuestros testimonios</h1>
+
+          <Testimonio 
+            nombre= 'Doggy'
+            pais='Suecia'
+            imagen='perro'
+            cargo='Jugador de Futbol'
+            empresa='Manchester'
+            testimonio='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec metus non nisi cursus aliquam non et leo. Sed vel mollis justo. Aenean ornare sed mi sollicitudin sagittis. Proin euismod lacinia nulla id rutrum. Vivamus sed ligula sapien. Phasellus vestibulum diam ac elit tincidunt convallis. Nunc eget nibh nec orci lobortis interdum. Suspendisse in lorem commodo, ornare lorem in, maximus ante. Suspendisse convallis augue eros, id congue turpis varius et. Morbi dui enim, egestas ut purus ut, lobortis pretium arcu. Duis maximus ante suscipit dignissim vestibulum. Etiam quis ligula dui. Curabitur fringilla aliquam viverra.'
+          />
+                    <Testimonio 
+            nombre= 'Wolfy'
+            pais='Italia'
+            imagen='lobo'
+            cargo='Desarrollador Front End'
+            empresa='Oracle'
+            testimonio='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec metus non nisi cursus aliquam non et leo. Sed vel mollis justo. Aenean ornare sed mi sollicitudin sagittis. Proin euismod lacinia nulla id rutrum. Vivamus sed ligula sapien. Phasellus vestibulum diam ac elit tincidunt convallis. Nunc eget nibh nec orci lobortis interdum. Suspendisse in lorem commodo, ornare lorem in, maximus ante. Suspendisse convallis augue eros, id congue turpis varius et. Morbi dui enim, egestas ut purus ut, lobortis pretium arcu. Duis maximus ante suscipit dignissim vestibulum. Etiam quis ligula dui. Curabitur fringilla aliquam viverra.'
+          />
+
+          <Testimonio 
+            nombre= 'Leo'
+            pais='Canada'
+            imagen='leon'
+            cargo='Ejecutivo de ventas'
+            empresa='Claro'
+            testimonio='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec metus non nisi cursus aliquam non et leo. Sed vel mollis justo. Aenean ornare sed mi sollicitudin sagittis. Proin euismod lacinia nulla id rutrum. Vivamus sed ligula sapien. Phasellus vestibulum diam ac elit tincidunt convallis. Nunc eget nibh nec orci lobortis interdum. Suspendisse in lorem commodo, ornare lorem in, maximus ante. Suspendisse convallis augue eros, id congue turpis varius et. Morbi dui enim, egestas ut purus ut, lobortis pretium arcu. Duis maximus ante suscipit dignissim vestibulum. Etiam quis ligula dui. Curabitur fringilla aliquam viverra.'
+          />
+
+      </div>
     </div>
   );
 }
---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../hojas-de-estilos/testimonio.css'
+
+function Testimonio(props){
+
+    return(
+
+        <div className='contenedor-testimonio'>
+        
+            <img className='imagen-testimonio'
+            src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+            alt='Foto de perro con traje' />
+
+            <div className='contenedor-texto-testimonio'>
+
+                <p className='nombre-testimonio'>
+                <strong>{props.nombre}</strong> en {props.pais}</p>
+                <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+                <p className='texto-testimonio'>"{props.testimonio}"</p>
+
+            </div>
+
+        </div>
+
+    );
+
+}
+
+export default Testimonio;
